02ebc1640f1fc7765c6a1e851b09aa0d976f722e
Parisa Mohebbi <100876263+ParisaMohebbi@users.noreply.github.com>
2025-02-08 15:54:04 -0600

    Update Apriori.py

--- Apriori.py
+++ Apriori.py
@@ -2,7 +2,7 @@
 Disease Cluster Analysis in Electronic Health Records: Insights into Mortality and Comorbidity Patterns
 Submitted to the IISE Annual Conference & Expo 2025
 Abstract ID: 6965
-Authors: Parisa Vaghfi Mohebbi, Ahmad Salehiyan
+Authors: Parisa Vaghfi Mohebbi, Ahmad Salehiyan, Akash Deep
 """
 
 # Import packages
